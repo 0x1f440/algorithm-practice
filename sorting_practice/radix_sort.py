fed45269de644c5d5ec5767daf8c9123dca637d2
Yura Lee <0x1f440@gmail.com>
2019-04-19 18:02:32 +0900

    Radix Sort에 MSD 구현 추가

--- sorting_practice/radix_sort.py
+++ sorting_practice/radix_sort.py
@@ -6,7 +6,7 @@
 # O(dn) (d는 데이터의 최대 자리수)
 
 
-def radix_sort(arr):
+def radix_lsd(arr):
     position = 1
 
     while True:
@@ -36,5 +36,42 @@ def radix_sort(arr):
             return arr
 
 
-test_case = list(map(int, input().split(',')))
-print("결과 : " + str(radix_sort(test_case)))
+def get_pos(arr, pos):
+    position = pos
+
+    if pos == 0:  # 맨 처음 호출 시 자리수를 계산합니다.
+        for i in arr:
+            if len(str(i)) > position:
+                position = len(str(i))
+
+        position = pow(10, position - 1)
+        print("------------------------------")
+        print("pos:", position, ", arr:", arr)
+
+    return position
+
+
+def radix_msd(arr, pos=0):
+    position = get_pos(arr, pos)
+
+    stack_list = [[] for _ in range(10)]
+
+    for number in arr:  # 각 자릿수에 맞게 스택에 삽입
+        div_num = number // position
+        radix = div_num % 10
+        stack_list[radix].append(number)
+
+    print(stack_list)
+
+    arr = []  # 재귀호출을 이용하여 정렬합니다
+    for numbers in stack_list:
+        if position >= 10 and numbers:
+            arr += radix_msd(numbers, position // 10)
+        else:
+            arr += numbers
+
+    return arr
+
+
+test_case = [544, 4902, 392, 1000, 1, 2, 4355, 567, 565]
+print("\n결과 : " + str(radix_msd(test_case)))
