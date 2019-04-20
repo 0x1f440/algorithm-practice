08349ef433267f20fbb6568aff4c65c6672cc98a
Yura Lee <0x1f440@gmail.com>
2019-04-21 01:17:47 +0900

    __delitem__ 메소드를 수정하여 해쉬테이블에 del 구현

--- data_structure/hashtable.py
+++ data_structure/hashtable.py
@@ -6,7 +6,7 @@ from hasher import pearson_hash
 
 
 class HashTable:
-    tombstone = [object(), None]
+    TOMBSTONE = [object(), None]
 
     def __init__(self):
         self.clear()
@@ -18,7 +18,10 @@ class HashTable:
             self.update(key, value)
 
     def __getitem__(self, item):
-        return self.search(item)
+        return self.get(item)
+
+    def __delitem__(self, key):
+        self.delete(key)
 
     def get_key(self, idx):
         return self.table[idx][0]
@@ -41,17 +44,17 @@ class HashTable:
                 raise KeyError(f"당신은 똑가튼 키를 입력해서 고먐미의 저주를 받아따!!! 중복된 키는 {key}이다!!!")
             idx += 1
 
-        self.table[idx] = [key, val]
+        self.table[idx] = (key, val)
         print(f"{idx}번 인덱스에 {key}: {val} 을 성공적으로 삽입했습니다")
 
     def delete(self, key):
-        self.table[self.find_slot(key)] = self.tombstone
+        self.table[self.find_slot(key)] = self.TOMBSTONE
 
     def update(self, key, val):
         print(f"{key}키의 값이 {val}로 바뀌었습니다")
-        self.table[self.find_slot(key)] = [key, val]
+        self.table[self.find_slot(key)] = (key, val)
 
-    def search(self, key):
+    def get(self, key):
         return self.table[self.find_slot(key)][1]
 
     def clear(self):
@@ -61,12 +64,12 @@ class HashTable:
 
 a = HashTable()
 a["key1"] = "val1"  # a.insert("key1", "val1")
-print(a["key1"])  # a.search("key1")
+print(a["key1"])  # a.get("key1")
 a["key1"] = "new_val1"  # a.update("key1", "new_val1")
-print(a["key1"])  # a.search("key1")
+print(a["key1"])  # a.get("key1")
 
-a.delete("key1")
-print(a.table[100] == a.tombstone)
+del a["key1"]  # a.delete("key1")
+print(a.table[100] == HashTable.TOMBSTONE)
 
 a.insert("key2", "val2")
-print(a.search("key2"))
+print(a.get("key2"))
