bda37d2fd47e9e21ebb4fa0c3b8e8acc1b8d8c4b
Yura Lee <0x1f440@gmail.com>
2019-07-25 21:02:40 +0900

    규칙 찾기에 가까운 문제. 카운팅 소트를 연습해봄

---
+++ 2551 - 두 대표 자연수/2551.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int nums[5000001];
+int counter[5000001];
+
+int main() {
+	cin >> n;
+	long long all_sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+		all_sum += nums[i];
+		if (nums[i] > m)
+			m = nums[i];
+	}
+
+	for (int i = 0; i < n; i++)
+		counter[nums[i]]++;
+
+	int idx = 0;
+	for (int i = 0; i <= m; i++)
+	{
+		if (counter[i] > 0) {
+			for (int j = 0; j < counter[i]; j++) {
+				nums[idx++] = i;
+			}
+		}
+	}
+
+	int avg = all_sum / n;
+
+	if (all_sum / n == (all_sum - 1) / n) {
+		long long f_power_sum = 0, s_power_sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			f_power_sum += abs(nums[i] - avg) * abs(nums[i] - avg);
+			s_power_sum += abs(nums[i] - (avg + 1)) * abs(nums[i] - (avg + 1));
+		}
+
+		if (f_power_sum > s_power_sum)
+			avg += 1;
+	}
+
+	cout << nums[(n - 1) / 2] << " " << avg;
+
+	return 0;
+}
